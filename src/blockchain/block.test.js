f5e4132fb6113eeeca4b56e8a9486e4fc470f26d
Henrique Koller <38964774+KollerZx@users.noreply.github.com>
2021-11-19 16:58:14 -0300

    testado geração de blocos e proof_of_work

---
+++ src/blockchain/block.test.js
@@ -0,0 +1,25 @@
+const Block = require('./Block')
+const { DIFFICULTY } = require('../config')
+
+
+describe('Block', () =>{
+    let data, lastBlock, block
+
+    beforeEach(() =>{
+        data = 'bar'
+        lastBlock = Block.genesis()
+        block = Block.mineBlock(lastBlock, data)
+    })
+
+    it('sets the `data` to match the input', () =>{
+        expect(block.data).toEqual(data)
+    })
+    it('sets the `lastHash` to match the hash of the last block', () =>{
+        expect(block.lastHash).toEqual(lastBlock.hash)
+    })
+    it('generates a hash that matches the difficulty', () =>{
+        expect(block.hash.substring(0 , DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY))
+
+        console.log(block.toString())
+    })
+})
